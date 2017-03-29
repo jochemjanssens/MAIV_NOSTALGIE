562510285b676afa719b060f863930ea850464f8
Jochem Janssens <janssens.jochem@gmail.com>
2017-03-29 20:07:44 +0200

    fixes #7 standaard nav

---
+++ src/js/nav.js
@@ -0,0 +1,22 @@
+const init = () => {
+  const menuitems = [`savedmylive`, `sexualhealing`, `midnight`, `year`, `dvd`];
+  menuitems.forEach(item => {
+    const $li = document.querySelector(`.menuitem-${item}`);
+    $li.addEventListener(`click`, clickHandler);
+  });
+};
+
+const clickHandler = e => {
+  const classname = e.currentTarget.className.replace(`menuitem-`, ``);
+  const $element = document.querySelector(`.${classname}`);
+  window.scroll({
+    top: $element.offsetTop,
+    left: 0,
+    behavior: `smooth`
+  });
+};
+
+export default () => {
+  init();
+  console.log(`halo`);
+};
