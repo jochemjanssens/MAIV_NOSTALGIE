28be6d1f90dee1bab436a7d0f7972ddd45b17be8
Jochem Janssens <janssens.jochem@gmail.com>
2017-03-29 15:22:13 +0200

    kleine bugfix js

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,16 @@
 
 /* eslint-disable react/jsx-filename-extension */
- import map from '../js/map.js';
+
  import React from 'react';
  import {render} from 'react-dom';
  import App from './containers/App';
+
  import nostaglieRadio from '../js/nostaglieRadio.js';
+ import map from '../js/map.js';
 
  const init = () => {
-   nostaglieRadio();
    map();
+   nostaglieRadio();
    render(
      <App />,
      document.querySelector(`.lyrics-react`)
