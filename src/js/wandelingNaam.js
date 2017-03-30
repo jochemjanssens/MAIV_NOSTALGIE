83584d81d7a1aecf697a911da637e2a1df73bf8d
DieterVanHertbruggen <dietervanhertbruggen8@gmail.com>
2017-03-30 11:45:10 +0200

    wandeling datum

--- src/js/wandelingNaam.js
+++ src/js/wandelingNaam.js
@@ -1,8 +1,11 @@
 const form = document.querySelector(`form`);
 const naam = document.querySelector(`.wandeling-confirmation`);
+const tijdsaanduiding = document.querySelector(`.tijdsaanduiding`);
+const date = new Date();
 
 const init = () => {
   form.addEventListener(`focusout`, submitHandler);
+  showDate();
 };
 
 const submitHandler = e => {
@@ -12,6 +15,20 @@ const submitHandler = e => {
   inputvalue.innerHTML = value;
   naam.classList.remove(`wandeling-confirmatio-hidden`);
   naam.innerHTML = `${`begin eraan ${  value}`} ! `;
+
+};
+
+const showDate = () => {
+  const currentTime = date.getHours();
+  if (currentTime >= 0 && currentTime <= 10) {
+    tijdsaanduiding.innerHTML = `Goedemorgen`;
+  }
+  else if (currentTime >= 11 && currentTime <= 13) {
+    tijdsaanduiding.innerHTML = `Goedemiddag`;
+  }
+  else if (currentTime >= 14 && currentTime <= 24) {
+    tijdsaanduiding.innerHTML = `Goedeavond`;
+  }
 };
 
 export default () => {
