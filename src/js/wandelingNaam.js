f5420c9614d6f94e3f13ffec92bb983dd501c33d
DieterVanHertbruggen <dietervanhertbruggen8@gmail.com>
2017-03-30 11:07:59 +0200

    wandeling naam confirmation

--- src/js/wandelingNaam.js
+++ src/js/wandelingNaam.js
@@ -1,7 +1,8 @@
 const form = document.querySelector(`form`);
+const naam = document.querySelector(`.wandeling-confirmation`);
 
 const init = () => {
-  form.addEventListener(`submit`, submitHandler);
+  form.addEventListener(`focusout`, submitHandler);
 };
 
 const submitHandler = e => {
@@ -9,6 +10,8 @@ const submitHandler = e => {
   const value = document.querySelector(`.input`).value;
   const inputvalue = document.querySelector(`.inputvalue`);
   inputvalue.innerHTML = value;
+  naam.classList.remove(`wandeling-confirmatio-hidden`);
+  naam.innerHTML = `${`begin eraan ${  value}`} ! `;
 };
 
 export default () => {
